7da24307e761a63206092a702b74acde9232337a
Luciano <lunick997@gmail.com>
2023-11-12 14:41:20 +0100

    commit

---
+++ es4c#/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Inserisci del testo:");
+string? inputText = Console.ReadLine();
+
+string reversedText = ReverseText(inputText);
+
+Console.WriteLine($"Testo al contrario: {reversedText}");
+
+    static string ReverseText(string text)
+    {
+        char[] charArray = text.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
